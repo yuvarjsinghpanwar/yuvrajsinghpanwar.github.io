c5bdc858e50288c8eacc72435b100498ba0d2104
Your Name <yuvrajsinghpanwar880@gmail.com>
2023-06-17 17:53:43 +0530

    Dynamic age

---
+++ assets/js/my.js
@@ -0,0 +1,15 @@
+window.onload = addGreetingText; 
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function addGreetingText() {
+    document.getElementById("greeting_txt").innerHTML = "Hello! I am " + getAge("08/12/1999") + " years old, My nickname is Yashu.";
+}
